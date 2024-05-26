60d894fc2a8a5904b7c926618cc9a25438dde3f5
wrosner <wrosner@kellerkind.rosner.lokal>
2024-05-26 19:41:05 +0200

    stripped code volume by keeping objects

--- test_GPPart.py
+++ test_GPPart.py
@@ -3,3 +3,32 @@
 #
 # (c) Wolfgang Rosner 2024 - wolfagngr@github.com
 # License: LGPL 2+
+
+import FreeCAD
+import Part
+import ManipulatorCMD
+# import Commands
+
+doc = App.newDocument()
+
+objPart = doc.addObject('App::Part','Part')
+objPart.Label = 'Part'
+# App.ActiveDocument.recompute()
+
+objBox = doc.addObject("Part::Box","Box")
+objBox.Label = "Cube"
+objPart.addObject(objBox)
+# App.ActiveDocument.recompute()
+
+objCone = doc.addObject("Part::Cone","Cone")
+objCone.Label = "Cone"
+objPart.addObject(objCone)
+objCone.Radius2 = '1 mm'
+objCone.Height = '7.777 mm'
+objCone.Angle = '237 deg'
+
+# App.ActiveDocument.recompute()
+
+
+
+doc.recompute(None,True,True)
