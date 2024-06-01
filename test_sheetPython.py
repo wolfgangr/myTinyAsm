2591065551e55a8dcc50e5f4c802e42237e63fc1
wrosner <wrosner@kellerkind.rosner.lokal>
2024-06-01 22:41:07 +0200

    toggling ref constraints to 0DoF

--- test_sheetPython.py
+++ test_sheetPython.py
@@ -64,13 +64,17 @@ sketch.setDatum(7,App.Units.Quantity('1.254000 mm'))
 sketch.renameConstraint(7, u'b')
 
 sketch.addConstraint(Sketcher.Constraint('Distance',2,1.565187))
+sketch.toggleDriving(8)
 sketch.renameConstraint(8, u'c')
+
 
 sketch.addConstraint(Sketcher.Constraint('Angle',2,1,1,2,1.567153))
 # sketch.setDatum(9,App.Units.Quantity('89.791000 deg'))
+sketch.toggleDriving(9)
 sketch.renameConstraint(9, u'_A')
 
 sketch.addConstraint(Sketcher.Constraint('Angle',0,2,2,2,0.676887))
+sketch.toggleDriving(10)
 sketch.renameConstraint(10, u'_B')
 
 
