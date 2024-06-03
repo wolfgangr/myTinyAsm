c3628552b4b01adf194d59db5059f14a6d8c638a
wrosner <wrosner@kellerkind.rosner.lokal>
2024-06-04 01:04:26 +0200

    start drafting sheetPyCEvalidator class

--- spEvalidate.py
+++ spEvalidate.py
@@ -4,9 +4,21 @@
 # .../FreeCAD/Macro/dev$ git clone https://github.com/yaroslaff/evalidate.git
 
 import dev.evalidate.evalidate as evalidate
+import dev.myTinyAsm.findFunctions
 
 # anything is forbidden if not explicitly allowed:
 # start by generic math formulae and test what is missing
 # sPyMod_model = evalidate.mult_eval_model.clone()
 sPyMod_model = evalidate.base_eval_model.clone()
 sPyMod_model.nodes.extend(['Mult', 'Call', 'Attribute'] )
+
+class sheetPyCEvalidator:
+    """ implement evalidate and associated model for sheetPythonCustom """
+    def __init__(self, sheet):
+        self.sheet = sheet
+        self.model = evalidate.base_eval_model.clone()
+        self.model.nodes.extend(['Mult', 'Call', 'Attribute'] )
+
+
+
+
