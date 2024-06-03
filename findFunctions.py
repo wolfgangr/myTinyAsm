f4b5b105bea3f7822378af5b56c309f8dc909a98
wrosner <wrosner@kellerkind.rosner.lokal>
2024-06-03 20:41:14 +0200

    try with custom sys.path

--- findFunctions.py
+++ findFunctions.py
@@ -1,14 +1,32 @@
 # from dev.myTinyAsm.trianglesolver import solve
 # from dev.myTinyAsm.sheetPyMods_base import *
 
+import importlib
+
 def findFuncs(dirs, files, funcs):
 
     ## to be replaced later
     # from dev.myTinyAsm.trianglesolver import solve
     # from dev.myTinyAsm.sheetPyMods_base import select
     # from dev.myTinyAsm.sheetPyMods_base import select_args
-    import dev.myTinyAsm.sheetPyMods_base
-    import dev.myTinyAsm.trianglesolver
+    pass
+
+    sp_bck = sys.path # backup since we are fiddling
+    sys.path = []
+    sys.path.append('/home/wrosner/.local/share/FreeCAD/Macro')
+    # my_trislv=importlib.machinery.PathFinder.find_module('dev.myTinyAsm.trianglesolver', path='/home/wrosner/.local/share/FreeCAD/Macro')
+
+    # my_trislv1=importlib.machinery.PathFinder.find_module('dev.myTinyAsm.trianglesolver', path=['/home/wrosner/.local/share/FreeCAD/Macro'])
+
+    # my_trislv2=importlib.machinery.PathFinder.find_module('dev.myTinyAsm.trianglesolver')
+
+    try:
+        import dev.myTinyAsm.sheetPyMods_base
+        import dev.myTinyAsm.trianglesolver
+    except:
+        print ("exception in import")
+
+    sys.path = sp_bck # restore previous sys.path
 
     # rv = locals()
     rv = {}
